1699e95ea691a0fe627a18fdf7f4c74730f56d4d
Joana Levcheva <61312176+JoeJoe1313@users.noreply.github.com>
2020-10-04 17:58:44 +0300

    better version

--- permutationCipherWithGrid.swift
+++ permutationCipherWithGrid.swift
@@ -1,30 +1,37 @@
-let firstBox: [Character] = ["*", "a"]
-let secondBox: [Character] = ["*", "m"]
-let thirdBox: [Character] = ["*", "a", "i"]
-let fourthBox: [Character] = ["*", "h", "k"]
-let fifthBox: [Character] = ["*", "e", "a"]
-let sixthBox: [Character] = ["*", "o", "s"]
+let firstBox: [String] = ["*", "a"]
+let secondBox: [String] = ["*", "m"]
+let thirdBox: [String] = ["*", "a", "i"]
+let fourthBox: [String] = ["*", "h", "k"]
+let fifthBox: [String] = ["*", "e", "a"]
+let sixthBox: [String] = ["*", "o", "s"]
+
+func cartsian_product(lhs: [String], rhs: [String]) -> [String] {
+    lhs.flatMap { left in
+        rhs.map { right in
+            "\(left)\(right)"
+        }
+    }
+}
+
+func multiple_cartesian_product(input: [[String]]) -> [String] {
+    var result = [""]
+    for arr in input {
+        result = cartsian_product(lhs: result, rhs: arr)
+    }
+    return result
+}
+
+var myArray: [String] = multiple_cartesian_product(input: [firstBox, secondBox, thirdBox, fourthBox, fifthBox, sixthBox])
 
 var myCount: Int
-
-for first in firstBox {
-    for second in secondBox {
-        for third in thirdBox {
-            for fourth in fourthBox {
-                for fifth in fifthBox {
-                    for sixth in sixthBox {
-                        myCount = 0
-                        for box in "\(first)\(second)\(third)\(fourth)\(fifth)\(sixth)" {
-                            if box == "*" {
-                                myCount += 1
-                            }
-                        }
-                        if myCount == 0 || myCount == 1 {
-                                print("\(first)\(second)\(third)\(fourth)\(fifth)\(sixth)")
-                        }
-                    }
-                }
-            }
+for element in myArray {
+    myCount = 0
+    for box in element {
+        if box == "*" {
+            xsmyCount += 1
         }
+    }
+    if myCount == 0 || myCount == 1 {
+        print(element)
     }
 }
